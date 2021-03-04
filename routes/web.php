d3674b577e1236c9999bcb6a8780057f53381c68
Nguyễn Thiên <4708693-thien.nguyen2@users.noreply.gitlab.com>
2021-03-04 23:21:07 +0700

    create controller and show data

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'customers'], function () {
+    Route::get('/',[\App\Http\Controllers\CustomerController::class, 'index'])->name('customers.index');
 });
